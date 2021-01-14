c2780692b448214bee2b963b640099abb1c939d1
Khanh Le <khanh.le02@base.vn>
2021-01-14 14:39:17 +0700

    index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 declare module "react-native-voip-push-notification" {
   export default class RNVoipPushNotification {
-    RNVoipPushRemoteNotificationsRegisteredEvent: string;
-    RNVoipPushRemoteNotificationReceivedEvent: string;
-    RNVoipPushDidLoadWithEvents: string;
+    static get RNVoipPushRemoteNotificationsRegisteredEvent(): string;
+    static get RNVoipPushRemoteNotificationReceivedEvent(): string;
+    static get RNVoipPushDidLoadWithEvents(): string;
     static registerVoipToken(): void;
     static registerVoipToken(): void;
     static getVoipToken(): Promise<string>;
